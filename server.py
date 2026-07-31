250910f713dbed59fe4a33bdbb5959fe9209ce53
alvster98-stack <alvster98@gmail.com>
2026-07-31 21:22:56 +0800

    Update server.py

--- server.py
+++ server.py
@@ -1,1 +1,27 @@
+from flask import Flask, jsonify
+import json
 
+app = Flask(__name__)
+
+
+@app.route("/manifest.json")
+def manifest():
+    with open("manifest.json") as f:
+        return jsonify(json.load(f))
+
+
+@app.route("/catalog/<type>/<id>.json")
+def catalog(type, id):
+
+    if type == "movie":
+        file = "catalog/movie/netflix-malaysia.json"
+
+    else:
+        file = "catalog/series/netflix-malaysia.json"
+
+    with open(file) as f:
+        return jsonify(json.load(f))
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=10000)
